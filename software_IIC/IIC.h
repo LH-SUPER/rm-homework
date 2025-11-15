65498e26452a9da74cdd6d275588f439ea7feee5
LH-SUPER <3149441931@qq.com>
2025-11-15 15:34:40 +0800

    Add IIC header file with function declarations

--- software_IIC/IIC.h
+++ software_IIC/IIC.h
@@ -1,1 +1,13 @@
+#ifndef __SOFTIIC_H
+#define __SOFTIIC_H
 
+#include "main.h"
+#include "stm32f1xx_hal.h"
+
+void IIC_Start(void);
+void IIC_Stop(void);
+void IIC_SendByte(uint8_t data);
+uint8_t IIC_ReadAck(void);
+void IIC_SendData(uint8_t dat);
+
+#endif
