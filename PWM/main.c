2a8203b2f127a09f407c63f300689335a3f7e8a9
LH-SUPER <3149441931@qq.com>
2025-11-15 16:50:15 +0800

    Update main.c

--- PWM/main.c
+++ PWM/main.c
@@ -1,18 +1,6 @@
 //其实没有实操，理论学完之后debugger的error为0就没管了（因为买的江协套件有外设缺失
-
-
-#include "main.h"
-
-TIM_HandleTypeDef htim3;  // PWM
-TIM_HandleTypeDef htim2;  // Encoder
-
-int speed = 0;            // 在调试模式看变量
-int pwmValue = 400;       // 默认
-
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_TIM3_Init(void);
-static void MX_TIM2_Init(void);
+int speed = 0;
+int pwmValue = 400;
 
 int main(void)
 {
@@ -22,19 +10,20 @@ int main(void)
     MX_GPIO_Init();
     MX_TIM3_Init();
     MX_TIM2_Init();
+
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
     HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
 
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwmValue); // 给一个 PWM
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwmValue);
 
     while (1)
     {
-        static uint32_t lastCnt = 0;
-        uint32_t nowCnt = __HAL_TIM_GET_COUNTER(&htim2);
+        static int last = 0;
+        int now = __HAL_TIM_GET_COUNTER(&htim2);
 
-        speed = nowCnt - lastCnt;
-        lastCnt = nowCnt;
+        speed = now - last;
+        last = now;
 
-        HAL_Delay(100);  // 缓
+        HAL_Delay(100);
     }
 }
