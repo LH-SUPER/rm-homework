3078c4c1ec409e7690b9ab61dc29ae46d9ecd44a
LH-SUPER <3149441931@qq.com>
2025-11-15 16:53:35 +0800

    Update main.c

--- PWM/main.c
+++ PWM/main.c
@@ -1,6 +1,6 @@
 //其实没有实操，理论学完之后debugger的error为0就没管了（因为买的江协套件有外设缺失
 int speed = 0;
-int pwmValue = 400;
+int lastCount = 0;
 
 int main(void)
 {
@@ -8,21 +8,19 @@ int main(void)
     SystemClock_Config();
 
     MX_GPIO_Init();
-    MX_TIM3_Init();
-    MX_TIM2_Init();
+    MX_TIM3_Init(); // PWM
+    MX_TIM2_Init(); // Encoder
 
     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
     HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
 
-    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwmValue);
+    __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 400);
 
     while (1)
     {
-        static int last = 0;
         int now = __HAL_TIM_GET_COUNTER(&htim2);
-
-        speed = now - last;
-        last = now;
+        speed = now - lastCount; // 速度
+        lastCount = now;
 
         HAL_Delay(100);
     }
